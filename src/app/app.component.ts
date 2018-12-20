7c6494722240e11ea65f8ce370a730838cede885
aurelie <aurelie.cissokho@gmail.com>
2018-12-20 22:18:09 +0100

    Création de l'array des posts dans le AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'blogAngular';
+export class AppComponent implements OnInit {
+
+  posts: Array<Object>;
+
+  constructor() {
+    this.posts = [
+      {
+        title: 'Article 1',
+        content: 'Culpa obsecrans graviter ut inpetraret se ab innocentium graviter gemens innocentium ut ut gemens omni illas omni omni non quieti provincias coalitos proiectare homines pro se perquisitor pro gemens homines.',
+        loveIts: 0,
+        created_at: new Date()
+      },
+      {
+        title: 'Article 2',
+        content: 'Culpa obsecrans graviter ut inpetraret se ab innocentium graviter gemens innocentium ut ut gemens omni illas omni omni non quieti provincias coalitos proiectare homines pro se perquisitor pro gemens homines.',
+        loveIts: 0,
+        created_at: new Date()
+      },
+      {
+        title: 'Article 3',
+        content: 'Culpa obsecrans graviter ut inpetraret se ab innocentium graviter gemens innocentium ut ut gemens omni illas omni omni non quieti provincias coalitos proiectare homines pro se perquisitor pro gemens homines.',
+        loveIts: 0,
+        created_at: new Date()
+      },
+      {
+        title: 'Article 4',
+        content: 'Culpa obsecrans graviter ut inpetraret se ab innocentium graviter gemens innocentium ut ut gemens omni illas omni omni non quieti provincias coalitos proiectare homines pro se perquisitor pro gemens homines.',
+        loveIts: 0,
+        created_at: new Date()
+      }
+    ]
+  }
+
+  ngOnInit() {
+  }
 }
