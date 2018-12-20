bf4f51fa8a05a2f57fa6c357cc65a7a759bcb505
aurelie <aurelie.cissokho@gmail.com>
2018-12-20 22:23:40 +0100

    Affichage dans PostListComponent d'un PostListItemComponent pour chaque post dans l'array qu'il a reçu

--- src/app/post-list-item/post-list-item.component.ts
+++ src/app/post-list-item/post-list-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-post-list-item',
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-list-item.component.scss']
 })
 export class PostListItemComponent implements OnInit {
+
+  @Input() postTitle: string;
+  @Input() postContent: string;
+  @Input() postDate: Date;
+  @Input() loveIts: number;
 
   constructor() { }
 
