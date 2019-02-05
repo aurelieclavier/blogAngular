03cf4110baa329b3a7e0e725ea79eb6552a7b04e
aurelie <aurelie.cissokho@gmail.com>
2019-02-05 21:35:08 +0100

    modification du routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,7 +18,7 @@ import { NewPostComponent } from './new-post/new-post.component';
 
 const appRoutes: Routes = [
   { path: 'posts', component: PostComponent },
-  { path: 'posts/new', component: NewPostComponent },
+  { path: 'new', component: NewPostComponent },
   { path: '', redirectTo: 'posts', pathMatch: 'full' },
   { path: '**', redirectTo: 'posts' }
 ];
